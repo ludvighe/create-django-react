a580f53feed8cb012816301c43d6f1ded1c4308c
luh <diskmaskin@github.com>
2021-08-16 22:23:57 +0200

    Update script & README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Create django-react integrated app
+
+## Why?
+
+Just for quality of life and for the fun of it.
+
+## Use
+
+```shell
+1. Download or clone repo
+2. run create_django_react.sh
+3. You're all set
+```
--- create_django_react.sh
+++ create_django_react.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
 
+# Consts
 NC="\033[0m"
 GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
 RED="\033[0;31m"
 
+NAME=$1
+
+SCRIPT_ROOT_PATH="$(pwd)"
+LOG="$SCRIPT_ROOT_PATH/LOG"
+
+# Script setup
+if [ -f "$SCRIPT_ROOT_PATH/create_django_react.sh" ]; then
+  run=1
+else 
+  echo "Please call this script from the same folder" && exit
+fi
+
+while [ -z "$NAME" ]; do
+  echo "Please enter the name of the project"
+  read NAME
+done
+
+cleanup_exit () {
+  if [ -f "$LOG" ]; then
+    cat $LOG
+    rm $LOG
+  fi
+  echo "Exiting"
+  exit $run
+}
+
+# Function wrapper to catch stdout and stderr and put in $LOG
+fn () {
+  [ $run == 1 ] && $1 &>$LOG || run=0
+}
+
+touch $LOG || cleanup_exit
+fn "rm .git"
+
 # Check dependencies
 echo -e "Checking dependencies..."
 
-dependencies=("python3" "pip3" "wget" "django-admin" "npm" "npx")
+dependencies=("python3" "pip3" "pipenv" "wget" "django-admin" "npm" "npx")
 dependencies_length=$(wc -w <<< ${dependencies[@]})
 
 run=1
@@ -26,45 +61,125 @@ do
     exit
 done
 
-# Script setup
-SCRIPT_ROOT_PATH="$(pwd)/$1_wrapper"
-LOG="$SCRIPT_ROOT_PATH/LOG"
-run=1
 
-cleanup_exit () {
-  if [ -f "$LOG" ]; then
-    cat $LOG
-    rm $LOG
-  fi
-  echo "Exiting"
-  exit
-}
-
-# Function wrapper to catch stdout and stderr and put in $LOG
-fn () {
-  [ $run == 1 ] && $1 &>$LOG || run=0
-}
-
-mkdir $SCRIPT_ROOT_PATH && cd $SCRIPT_ROOT_PATH && touch $LOG || cleanup_exit
+echo ""
 
 
-
-# Django setup
-run=1
-echo -en "Django:$YELLOW Setting up... $NC\r"
-fn "django-admin startproject $1"
-cd $1
+# Django - setup
+title="[1/2] Django - setup:"
+echo -en "$title$YELLOW Setting up... $NC\r"
+fn "django-admin startproject $NAME"
+fn "cd $NAME"
 fn "django-admin startapp api"
-# fn "django-admin startapp frontend"
 
-[ $run == 0 ] && echo -e "Django:$RED Failed $NC        " && echo -e "$RED""Could not set up django$NC" && cleanup_exit
-[ $run == 1 ] && echo -e "Django:$GREEN Done $NC        "
+[ $run == 0 ] && echo -e "$title$RED Failed $NC        " && echo -e "$RED""Could not set up django$NC" && cleanup_exit
+[ $run == 1 ] && echo -e "$title$GREEN Done $NC        "
 
-# React setup
-run=1
-echo -en "React:$YELLOW Setting up... $NC\r"
-# fn "npx create-react-app frontend"
-[ $run == 0 ] && echo -e "React:$RED Failed $NC        " && echo -e "$RED""Could not set up react$NC" && cleanup_exit
-[ $run == 1 ] && echo -e "React:$GREEN Done $NC        "
 
-cleanup_exit
+
+# Pipenv - setup
+title="[2/2] Django - pipenv setup"
+echo -en "$title$YELLOW Setting up... $NC\r"
+
+cd "$SCRIPT_ROOT_PATH/$NAME"
+fn "pipenv install django"
+
+[ $run == 0 ] && echo -e "$title$RED Failed $NC         " && echo -e "$RED""Could not set up Django/React integration$NC" && cleanup_exit
+[ $run == 1 ] && echo -e "$title$GREEN Done $NC         "
+
+
+
+# React - setup
+title="[1/3] React - setup:"
+echo -en "$title$YELLOW Setting up... $NC\r"
+
+cp -r "$SCRIPT_ROOT_PATH/frontend" "$SCRIPT_ROOT_PATH/$NAME" || run=0
+
+[ $run == 0 ] && echo -e "$title$RED Failed $NC        " && echo -e "$RED""Could not set up react$NC" && cleanup_exit
+[ $run == 1 ] && echo -e "$title$GREEN Done $NC        "
+
+
+
+# React - installing npm packages
+title="[2/3] React - installing npm packages:"
+echo -en "$title$YELLOW Installing... $NC\r"
+cd "$SCRIPT_ROOT_PATH/$NAME/frontend"
+fn "npm install"
+
+[ $run == 0 ] && echo -e "$title$RED Failed $NC        " && echo -e "$RED""Could not set up react$NC" && cleanup_exit
+[ $run == 1 ] && echo -e "$title$GREEN Done $NC        "
+
+
+
+# React - build
+title="[3/3] React - build:"
+echo -en "$title$YELLOW Building... $NC\r"
+
+fn "npm run build"
+
+[ $run == 0 ] && echo -e "$title$RED Failed $NC        " && echo -e "$RED""Could not set up react$NC" && cleanup_exit
+[ $run == 1 ] && echo -e "$title$GREEN Done $NC        "
+
+
+echo ""
+
+
+# Django / React Integrations
+title="Django/React - integration:"
+echo -en "$title$YELLOW Integrating... $NC\r"
+
+## settings.py - TEMPLATE, STATICFILES
+cd "$SCRIPT_ROOT_PATH/$NAME/$NAME"
+input=$(cat settings.py)
+search="'DIRS': [],"
+replace="'DIRS': [ os.path.join(BASE_DIR, 'frontend/build') ],"
+output="${input//"$search"/"$replace"}"
+
+search="from pathlib import Path"
+replace="from pathlib import Path\nimport os"
+output="${output//"$search"/"$replace"}"
+
+search="STATIC_URL = '/static/'"
+replace="STATIC_URL = '/static/'\n\nSTATICFILES_DIRS = [ os.path.join(BASE_DIR, 'frontend/build/static') ]\n"
+output="${output//"$search"/"$replace"}"
+
+echo -e "$output" > settings.py || run=0
+
+## urls.py - TemplateView
+cd "$SCRIPT_ROOT_PATH/$NAME/$NAME"
+input=$(cat urls.py)
+search="from django.urls import path"
+replace="from django.urls import path\nfrom django.views.generic import TemplateView"
+output="${input//"$search"/"$replace"}"
+
+search="path('admin/', admin.site.urls),"
+replace="path('admin/', admin.site.urls),\n    path('', TemplateView.as_view(template_name='index.html'))"
+output="${output//"$search"/"$replace"}"
+
+echo -e "$output" > urls.py || run=0
+
+[ $run == 0 ] && echo -e "$title$RED Failed $NC         " && echo -e "$RED""Could not set up Django/React integration$NC" && cleanup_exit
+[ $run == 1 ] && echo -e "$title$GREEN Done $NC         "
+
+
+
+# Git - setup
+title="Git:"
+echo -en "$title$YELLOW Setting up... $NC\r"
+
+cd "$SCRIPT_ROOT_PATH/$NAME"
+fn "git init"
+cp $SCRIPT_ROOT_PATH/.gitignore core/ || run=0
+
+[ $run == 0 ] && echo -e "$title$RED Failed $NC        " && echo -e "$RED""Could not set up react$NC" && cleanup_exit
+[ $run == 1 ] && echo -e "$title$GREEN Done $NC        "
+
+echo ""
+
+# Cleanup
+echo -en "$YELLOW""Cleaning up...""$NC\r"
+cd $SCRIPT_ROOT_PATH
+fn "rm -r frontend"
+fn "rm LOG .gitignore README.md $0"
+
+echo -e "$GREEN""You're all set up!""$NC"
